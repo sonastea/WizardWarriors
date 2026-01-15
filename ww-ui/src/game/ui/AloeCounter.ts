39fc2cafde7f579a9d86d4d9504ffc1173e0ab10
sonastea <sonastea@gmail.com>
2026-01-15 04:31:12 -0800

    feat: update aloe counter icon to use the texture from the spritesheet

--- ww-ui/src/game/ui/AloeCounter.ts
+++ ww-ui/src/game/ui/AloeCounter.ts
@@ -1,5 +1,7 @@
 import { GameObjects, Scene } from "phaser";
 
+const ALOE_FRAME = 65;
+
 interface AloeCounterConfig {
   /** Margin from the edge of the screen */
   margin?: number;
@@ -20,7 +22,7 @@ export class AloeCounter {
   private config: Required<AloeCounterConfig>;
   private container: GameObjects.Container;
   private background: GameObjects.Graphics;
-  private icon: GameObjects.Graphics;
+  private icon: GameObjects.Sprite;
   private countText: GameObjects.Text;
   private currentCount = 0;
 
@@ -33,71 +35,35 @@ export class AloeCounter {
     this.container.setDepth(1001);
 
     const size = this.config.iconSize;
+    const padding = 6;
+    const boxWidth = size + 28;
+    const boxHeight = size;
 
     this.background = this.scene.add.graphics();
     this.background.fillStyle(0x000000, 0.6);
-    this.background.fillRoundedRect(0, 0, size + 28, size, 6);
+    this.background.fillRoundedRect(0, 0, boxWidth, boxHeight, 6);
     this.background.lineStyle(1, 0x7cb342, 0.4);
-    this.background.strokeRoundedRect(0, 0, size + 28, size, 6);
+    this.background.strokeRoundedRect(0, 0, boxWidth, boxHeight, 6);
 
-    this.icon = this.scene.add.graphics();
-    this.drawAloe(this.icon, size / 2, size / 2, size);
-
-    this.countText = this.scene.add.text(size + 4, size / 2, "0", {
+    this.countText = this.scene.add.text(padding + 2, boxHeight / 2, "0", {
       fontSize: "14px",
       fontStyle: "bold",
       color: "#ffffff",
     });
     this.countText.setOrigin(0, 0.5);
 
+    this.icon = this.scene.add.sprite(
+      boxWidth - padding - 12,
+      boxHeight / 2,
+      "multiplayer-sheet",
+      ALOE_FRAME
+    );
+    this.icon.setScale(1.25);
+
     this.container.add([this.background, this.icon, this.countText]);
 
     this.updatePosition();
     this.scene.scale.on("resize", this.updatePosition, this);
-  }
-
-  /**
-   * Draw an aloe vera icon using graphics
-   */
-  private drawAloe(
-    graphics: GameObjects.Graphics,
-    centerX: number,
-    centerY: number,
-    size: number
-  ): void {
-    const leafLength = size * 0.4;
-    const leafWidth = size * 0.12;
-
-    const leafAngles = [-0.4, -0.15, 0.15, 0.4];
-
-    for (const angleOffset of leafAngles) {
-      const angle = -Math.PI / 2 + angleOffset; // Point upward with spread
-
-      graphics.fillStyle(0x7cb342, 1);
-      graphics.beginPath();
-
-      const tipX = centerX + Math.cos(angle) * leafLength;
-      const tipY = centerY + Math.sin(angle) * leafLength;
-      const baseLeftX = centerX + Math.cos(angle + Math.PI / 2) * leafWidth;
-      const baseLeftY = centerY + Math.sin(angle + Math.PI / 2) * leafWidth;
-      const baseRightX = centerX + Math.cos(angle - Math.PI / 2) * leafWidth;
-      const baseRightY = centerY + Math.sin(angle - Math.PI / 2) * leafWidth;
-
-      graphics.moveTo(tipX, tipY);
-      graphics.lineTo(baseLeftX, baseLeftY);
-      graphics.lineTo(baseRightX, baseRightY);
-      graphics.closePath();
-      graphics.fillPath();
-
-      graphics.lineStyle(1, 0x9ccc65, 0.8);
-      graphics.beginPath();
-      graphics.moveTo(centerX, centerY);
-      graphics.lineTo(tipX, tipY);
-      graphics.strokePath();
-    }
-
-    graphics.fillStyle(0x558b2f, 1);
-    graphics.fillCircle(centerX, centerY + size * 0.1, size * 0.1);
   }
 
   private updatePosition(): void {
